e3d64edc828139f5f2cd4c183209cc3eeee0e8ff
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2025-01-26 17:29:23 +0530

    Added utils

---
+++ operate/utils/operating_system.py
@@ -0,0 +1,56 @@
+import pyautogui
+import platform
+import time
+import math
+
+from utils.misc import convert_percent_to_decimal
+
+
+class OperatingSystem:
+    def write(self, content):
+        try:
+            content = content.replace("\\n", "\n")
+            for char in content:
+                pyautogui.write(char)
+        except Exception as e:
+            print("[OperatingSystem][write] error:", e)
+
+    def press(self, keys):
+        try:
+            for key in keys:
+                pyautogui.keyDown(key)
+            time.sleep(0.1)
+            for key in keys:
+                pyautogui.keyUp(key)
+        except Exception as e:
+            print("[OperatingSystem][press] error:", e)
+
+    def mouse(self, click_detail):
+        try:
+            x = convert_percent_to_decimal(click_detail.get("x"))
+            y = convert_percent_to_decimal(click_detail.get("y"))
+
+            if click_detail and isinstance(x, float) and isinstance(y, float):
+                self.click_at_percentage(x, y)
+
+        except Exception as e:
+            print("[OperatingSystem][mouse] error:", e)
+
+    def click_at_percentage(
+        self,
+        x_percentage,
+        y_percentage,
+        duration=0.8,
+        circle_radius=50,
+        circle_duration=0.5,
+    ):
+        try:
+            screen_width, screen_height = pyautogui.size()
+            x_pixel = int(screen_width * float(x_percentage))
+            y_pixel = int(screen_height * float(y_percentage))
+
+            pyautogui.moveTo(x_pixel, y_pixel, duration=duration)
+
+            pyautogui.click(x_pixel, y_pixel)
+        except Exception as e:
+            print("[OperatingSystem][click_at_percentage] error:", e)
