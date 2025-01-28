9e14c7475c2ef5f726f95793ef6e29694d2bfe43
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2025-01-28 18:42:55 +0530

    Added double click instead of click and added logging

--- operate/utils/operating_system.py
+++ operate/utils/operating_system.py
@@ -2,12 +2,14 @@ import pyautogui
 import platform
 import time
 import math
+from loguru import logger
 
 from utils.misc import convert_percent_to_decimal
 
 
 class OperatingSystem:
     def write(self, content):
+        logger.info("Write function executed by OS.")
         try:
             content = content.replace("\\n", "\n")
             for char in content:
@@ -16,6 +18,7 @@ class OperatingSystem:
             print("[OperatingSystem][write] error:", e)
 
     def press(self, keys):
+        logger.info("Press function executed by OS.")
         try:
             for key in keys:
                 pyautogui.keyDown(key)
@@ -26,6 +29,7 @@ class OperatingSystem:
             print("[OperatingSystem][press] error:", e)
 
     def mouse(self, click_detail):
+        logger.info("Click function executed by OS.")
         try:
             x = convert_percent_to_decimal(click_detail.get("x"))
             y = convert_percent_to_decimal(click_detail.get("y"))
@@ -44,6 +48,7 @@ class OperatingSystem:
         circle_radius=50,
         circle_duration=0.5,
     ):
+        logger.info("Percentage calculated with click function executed by OS.")
         try:
             screen_width, screen_height = pyautogui.size()
             x_pixel = int(screen_width * float(x_percentage))
@@ -51,6 +56,8 @@ class OperatingSystem:
 
             pyautogui.moveTo(x_pixel, y_pixel, duration=duration)
 
+            pyautogui.click(x_pixel, y_pixel)
+            time.sleep(0.1)
             pyautogui.click(x_pixel, y_pixel)
         except Exception as e:
             print("[OperatingSystem][click_at_percentage] error:", e)
