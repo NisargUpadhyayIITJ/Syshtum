27a09665a613ded91c31521f4988f4d4736ce5a0
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-22 00:10:45 +0530

    add Layout component and integrate react-router-dom; update API calls for error handling

--- Interface/UI/server/server.js
+++ Interface/UI/server/server.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 
 const getData = async (data) => {
-    const response = await axios.post('http://127.0.0.1:8002/pipeline', {...data});
-    console.log(response);
+    try {
+        const response = await axios.post('http://127.0.0.1:8002/pipeline', {...data});
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+const SaveApiKey = async (data)=>{
+    const response = await axios.post('http://127.0.0.1:8002/enter_api', {...data});
+    console.log(response);  
     return response.data;
 }
 
 export {
-    getData
+    getData,
+    SaveApiKey
 };
